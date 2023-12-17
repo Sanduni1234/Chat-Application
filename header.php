46a9e1eb478b9b1c7260b1604c4426c49605dd68
I M S S HERATH <109812013+Sanduni1234@users.noreply.github.com>
2023-12-17 15:54:15 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html >
+<head>
+    
+    <title>Agro Chat</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <style>
+    body{
+        background-image:url('images/bg.jpg');
+        background-repeat: repeat;
+        background-size: cover;
+        width:100%;
+    }
+    .navbar-inverse {
+        background-color:darkgreen;
+        border-color: forestgreen; 
+        opacity: 0.8;
+        margin-left: 0px;
+        position: absolute;
+        width:100%;
+    }
+    .navbar-inverse .navbar-brand {
+        color: white;
+    }
+    a:hover{
+        color: #50546d;
+    }
+    .navbar-inverse .navbar-nav>li>a {
+        color: white;
+    }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+     
+    </div>
+    
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<a class="" href="index.php"><img src="images\logo.png" style="width: 220px; margin-top:40px;"></a>
+</body>
+</html>
